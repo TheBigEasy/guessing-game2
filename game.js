ed8c14e4b5b9eddeb15a209d034cec0b4bd8e99c
John Sanford <johnsanfordstudio@gmail.com>
2015-09-30 12:54:28 -0700

    add files to new repo

---
+++ game.js
@@ -0,0 +1,17 @@
+var question1 = prompt('What state did I grow up in?');
+console.log('user response to question 1 is: ' + question1);
+
+var question2 = prompt('What college did I go to?');
+console.log('user reponse to question 2 is: ' + question2);
+
+var question3 = prompt('How long have I been Married?');
+console.log('user response to question 3 is: ' + question3);
+
+
+
+ 
+
+
+alert("you guessed " + question1 + ".  I grew up in California");
+alert('I am sure your guess of ' + question2 + ' is probably incorrect.  I went to a small college in Santa Barbara called Brooks.');
+alert('Your guess was ' + question3 + ' .  I have been married for 18 years.');
