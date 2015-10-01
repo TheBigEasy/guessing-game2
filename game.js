370dd402955da63f9658ce9dc7c169bce1487a42
John Sanford <johnsanfordstudio@gmail.com>
2015-10-01 13:07:12 -0700

    created functions, called them, and put previous questions into them.

--- game.js
+++ game.js
@@ -6,39 +6,52 @@ var message;
 
 
 
-var question1 = prompt('What state did I grow up in?');
-if (question1.toUpperCase() === "CALIFORNIA" || question1.toUpperCase() === "C" || question1.toUpperCase() === "CA") {
+function ques1() {
+  var question1 = prompt('What state did I grow up in?');
+  if (question1.toUpperCase() === "CALIFORNIA" || question1.toUpperCase() === "C" || question1.toUpperCase() === "CA") {
 	alert('Great job ' + userName + '!  There are a lot of states to choose from.');
 	score += 1;
-} else {
+  } else {
 	alert('Sorry ' + userName + ' .  I was born in California.');
+  }
+  console.log('user response to question 1 is: ' + question1);
 }
-console.log('user response to question 1 is: ' + question1);
 
-
-var question2 = prompt('What college did I go to?');
-if (question2.toUpperCase() === 'BROOKS INSTITUTE OF PHOTOGRAPHY' || question2.toUpperCase() === 'BROOKS') {
+function ques2() {
+  var question2 = prompt('What college did I go to?');
+  if (question2.toUpperCase() === 'BROOKS INSTITUTE OF PHOTOGRAPHY' || question2.toUpperCase() === 'BROOKS') {
 	alert('Hey ' + userName + ' That was an amazing guess.  Not too many people know that.');
 	score += 1;
-} else {
+  } else {
 	alert('Good guess ' + userName + ' , but not right.  Try again!');
+  }
+  console.log('user reponse to question 2 is: ' + question2);
 }
-console.log('user reponse to question 2 is: ' + question2);
 
-
-
-var question3 = prompt('How long have I been Married?');
-if (question3 === '18 Years' || question3 === '18' || question3 === 'eighteen years') {
+function ques3() {
+  var question3 = prompt('How long have I been Married?');
+  if (question3 === '18 Years' || question3 === '18' || question3 === 'eighteen years') {
 	alert('Absolutely right ' + userName + ' Nice job.');
 	score += 1;
-} else {
+  } else {
 	alert('I know it\'s hard to guess ' + userName + '.  I\'ll give you a hint, it is more than 10.');
+  }
+
+  console.log('user response to question 3 is: ' + question3);
+
+
 }
 
-console.log('user response to question 3 is: ' + question3);
+ques1();
+ques2();
+ques3();
 
-message = 'Good job ' + userName + ', you got' + score + ' out of 3 correct!';
-document.write(message);
+
+
+
+
+// message = 'Good job ' + userName + ', you got' + score + ' out of 3 correct!';
+// document.write(message);
 
 
 
