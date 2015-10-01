96b501705070f4c8b2cde93495a5c7a8ebcde9f6
John Sanford <johnsanfordstudio@gmail.com>
2015-10-01 13:20:25 -0700

    moved alerts into innerHTML id's

--- game.js
+++ game.js
@@ -4,15 +4,17 @@ var userName = prompt('Enter your name here.')
 var score = 0;
 var message;
 
-
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
 
 function ques1() {
   var question1 = prompt('What state did I grow up in?');
   if (question1.toUpperCase() === "CALIFORNIA" || question1.toUpperCase() === "C" || question1.toUpperCase() === "CA") {
-	alert('Great job ' + userName + '!  There are a lot of states to choose from.');
+	one.innerHTML = 'Great job ' + userName + '!  There are a lot of states to choose from.';
 	score += 1;
   } else {
-	alert('Sorry ' + userName + ' .  I was born in California.');
+	one.innerHTML ='Sorry ' + userName + ' .  I was born in California.';
   }
   console.log('user response to question 1 is: ' + question1);
 }
@@ -20,10 +22,10 @@ function ques1() {
 function ques2() {
   var question2 = prompt('What college did I go to?');
   if (question2.toUpperCase() === 'BROOKS INSTITUTE OF PHOTOGRAPHY' || question2.toUpperCase() === 'BROOKS') {
-	alert('Hey ' + userName + ' That was an amazing guess.  Not too many people know that.');
+	two.inner.HTML = 'Hey ' + userName + ' That was an amazing guess.  Not too many people know that.';
 	score += 1;
   } else {
-	alert('Good guess ' + userName + ' , but not right.  Try again!');
+	two.innerHTML ='Good guess ' + userName + ' , but not right.  Try again!';
   }
   console.log('user reponse to question 2 is: ' + question2);
 }
@@ -31,10 +33,10 @@ function ques2() {
 function ques3() {
   var question3 = prompt('How long have I been Married?');
   if (question3 === '18 Years' || question3 === '18' || question3 === 'eighteen years') {
-	alert('Absolutely right ' + userName + ' Nice job.');
+	three.innerHTML = 'Absolutely right ' + userName + ' Nice job.';
 	score += 1;
   } else {
-	alert('I know it\'s hard to guess ' + userName + '.  I\'ll give you a hint, it is more than 10.');
+	three.innerHTML = 'I know it\'s hard to guess ' + userName + '.  I\'ll give you a hint, it is more than 10.';
   }
 
   console.log('user response to question 3 is: ' + question3);
