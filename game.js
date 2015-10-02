3074eba3270ca29e5cc7f9cdc8c24dfdba6354e0
John Sanford <johnsanfordstudio@gmail.com>
2015-10-02 13:44:30 -0700

    added images to correct answers via innerHTML

--- game.js
+++ game.js
@@ -10,11 +10,14 @@ var one = document.getElementById('one');
 var two = document.getElementById('two');
 var three = document.getElementById('three');
 var msg = document.getElementById('msg');
-
+var picOne = document.getElementById('picOnes');
+var picTwo = document.getElementById('picTwos');
+var picThree = document.getElementById('picThrees');
 function ques1() {
   var question1 = prompt(questions[0]);
   if (question1.toUpperCase() === answers[0] || question1.toUpperCase() === answers[1] || question1.toUpperCase() === answers[2]) {
 	one.innerHTML = 'Great job ' + userName + '!  There are a lot of states to choose from.';
+	picOne.innerHTML = '<img src="img/san_diego.jpeg" width="200"/>';
 	score += 1;
   } else {
 	one.innerHTML ='Sorry ' + userName + ' .  I was born in California.';
@@ -26,6 +29,7 @@ function ques2() {
   var question2 = prompt(questions[1]);
   if (question2.toUpperCase() === answers[3] || question2.toUpperCase() === answers[4]) {
 	two.innerHTML = 'Hey ' + userName + ' That was an amazing guess.  Not too many people know that.';
+	picTwo.innerHTML = '<img src="img/brooks.jpeg" width ="200"/>';
 	score += 1;
   } else {
 	two.innerHTML ='Good guess ' + userName + ' , but not right.  Try again!';
@@ -38,6 +42,7 @@ function ques3() {
   if (question3 === answers[5] || question3 === answers[6] || question3 === answers[7]) {
 	three.innerHTML = 'Absolutely right ' + userName + ' Nice job.';
 	score += 1;
+	picThree.innerHTML = '<img src="img/couple.jpeg" width ="200"/>';
   } else {
 	three.innerHTML = 'I know it\'s hard to guess ' + userName + '.  I\'ll give you a hint, it is more than 10.';
   }
